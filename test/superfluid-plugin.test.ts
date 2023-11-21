e998d04d1ccfc2210313dc082fe3e10a9ca7a371
Salman Dabbakuti <dabbakutisalman@gmail.com>
2023-11-21 10:05:05 +0530

    WIP: idav1 support, readme, tests

--- test/superfluid-plugin.test.ts
+++ test/superfluid-plugin.test.ts
@@ -1,11 +1,13 @@
 import { Web3, Web3Eth, Web3Context } from "web3";
 import { expect } from "chai";
-import { SuperfluidPlugin, CFAV1Forwarder, CFAV1, Host } from "../src";
+import { SuperfluidPlugin, CFAV1Forwarder, CFAV1, IDAV1, Host } from "../src";
 
 // Test data: Mumbai testnet
+// https://console.superfluid.finance/mumbai/protocol
 const rpcUrl = "https://rpc-mumbai.maticvigil.com";
 const cfav1ForwarderAddress = "0xcfA132E353cB4E398080B9700609bb008eceB125";
 const cfav1Address = "0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873";
+const idav1Address = "0x804348D4960a61f2d5F9ce9103027A3E849E09b8";
 const hostAddress = "0xEB796bdb90fFA0f28255275e16936D25d3418603";
 const token = "0x5d8b4c2554aeb7e86f387b4d6c00ac33499ed01f"; //fDAIx on Mumbai
 const sender = "0xc7203561EF179333005a9b81215092413aB86aE9";
@@ -20,6 +22,7 @@ describe("SuperfluidPlugin Tests", () => {
     expect(web3.superfluid.pluginNamespace).to.be.equal("superfluid");
     expect(web3.superfluid.cfav1Forwarder).to.be.a("function");
     expect(web3.superfluid.cfav1).to.be.a("function");
+    expect(web3.superfluid.idav1).to.be.a("function");
     expect(web3.superfluid.host).to.be.a("function");
   });
 
@@ -31,6 +34,7 @@ describe("SuperfluidPlugin Tests", () => {
     expect(web3Context.superfluid.pluginNamespace).to.be.equal("superfluid");
     expect(web3Context.superfluid.cfav1Forwarder).to.be.a("function");
     expect(web3Context.superfluid.cfav1).to.be.a("function");
+    expect(web3Context.superfluid.idav1).to.be.a("function");
     expect(web3Context.superfluid.host).to.be.a("function");
   });
 
@@ -42,6 +46,7 @@ describe("SuperfluidPlugin Tests", () => {
     expect(web3EthContext.superfluid.pluginNamespace).to.be.equal("superfluid");
     expect(web3EthContext.superfluid.cfav1Forwarder).to.be.a("function");
     expect(web3EthContext.superfluid.cfav1).to.be.a("function");
+    expect(web3EthContext.superfluid.idav1).to.be.a("function");
     expect(web3EthContext.superfluid.host).to.be.a("function");
   });
 });
@@ -50,6 +55,7 @@ describe("SuperfluidPlugin Method Tests", () => {
   let web3: Web3;
   let cfav1Forwarder: CFAV1Forwarder;
   let cfav1: CFAV1;
+  let idav1: IDAV1;
   let host: Host;
 
   before(() => {
@@ -58,6 +64,7 @@ describe("SuperfluidPlugin Method Tests", () => {
     cfav1Forwarder = web3.superfluid.cfav1Forwarder(cfav1ForwarderAddress);
     cfav1 = web3.superfluid.cfav1(cfav1Address);
     host = web3.superfluid.host(hostAddress);
+    idav1 = web3.superfluid.idav1(idav1Address);
   });
 
   it("should get flow info with forwarder", async () => {
@@ -81,6 +88,17 @@ describe("SuperfluidPlugin Method Tests", () => {
       .call();
     expect(timestamp.toString()).to.be.a("string");
     expect(flowRate.toString()).to.be.a("string");
+  });
+
+  it("should get subscription with idav1", async () => {
+    const indexId = 1;
+    const { units, exist, approved } = await idav1.methods
+      .getSubscription(token, sender, indexId, receiver)
+      .call();
+    // expect to have units, exist, approved
+    expect(units.toString()).to.be.a("string");
+    expect(exist).to.be.a("boolean");
+    expect(approved).to.be.a("boolean");
   });
 
   it("should check if address is trusted forwarder using host", async () => {
